539e99e5e584f29c25218186c101c0eea5a409d6
Carmen Shing <carmens@ensemble.com>
2022-04-07 11:18:25 -0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  overrides: [
+    {
+      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
